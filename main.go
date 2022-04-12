f6b9f287784eb42ab156f643b465c87fb932beba
Matheus Carneiro Guimaraes <matheusorienrac@gmail.com>
2022-04-12 01:57:14 +0100

    now has working session stuff

--- main.go
+++ main.go
@@ -10,8 +10,6 @@ import (
 	"strconv"
 
 	_ "github.com/lib/pq"
-	uuid "github.com/satori/go.uuid"
-	"golang.org/x/crypto/bcrypt"
 )
 
 var tpl *template.Template
@@ -49,7 +47,8 @@ func main() {
 	http.HandleFunc("/pokedex/update", update)
 	http.HandleFunc("/pokedex/delete", delete)
 	http.HandleFunc("/pokedex", pokedex)
-	http.HandleFunc("/login", pokedex)
+	http.HandleFunc("/login", login)
+	http.HandleFunc("/logout", logout)
 	http.HandleFunc("/signup", signup)
 	http.HandleFunc("/", index)
 	http.ListenAndServeTLS(":10443", "cert.pem", "key.pem", nil)
@@ -57,58 +56,11 @@ func main() {
 
 func index(res http.ResponseWriter, req *http.Request) {
 	// if LoggedIn, redirects to My Pokedex, if not request to log in and offer sign up button
+	if alreadyLoggedIn(req) {
+		http.Redirect(res, req, "/pokedex", http.StatusSeeOther)
+		return
+	}
 	tpl.ExecuteTemplate(res, "index.gohtml", nil)
-	cookie, err := req.Cookie("session")
-	if err != nil {
-		id := uuid.NewV4()
-		cookie = &http.Cookie{
-			Name:     "session",
-			Value:    id.String(),
-			Secure:   true,
-			HttpOnly: true,
-		}
-		http.SetCookie(res, cookie)
-	}
-	http.Redirect(res, req, "/pokedex", http.StatusSeeOther)
-}
-
-func signup(res http.ResponseWriter, req *http.Request) {
-	if req.Method == http.MethodGet {
-		tpl.ExecuteTemplate(res, "signup.gohtml", nil)
-		return
-	}
-
-	u := req.FormValue("username")
-	p := req.FormValue("password")
-	hashPassword, err := bcrypt.GenerateFromPassword([]byte(p), bcrypt.MinCost)
-	if err != nil {
-		http.Error(res, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-		return
-	}
-
-	q := `
-		INSERT INTO USERS (USERNAME, PASSWORD)
-		VALUES ($1, $2);
-	`
-	result, err := db.Exec(q, u, hashPassword)
-	if err != nil {
-		http.Error(res, err.Error(), http.StatusForbidden)
-		return
-	}
-	rowsAffected, err := result.RowsAffected()
-	if err != nil {
-		http.Error(res, err.Error(), http.StatusForbidden)
-		return
-
-	}
-	if rowsAffected > 0 {
-		tpl.ExecuteTemplate(res, "signup.gohtml", "Account creation successful")
-		return
-	} else {
-		res.Write([]byte("something went wrong."))
-
-	}
-
 }
 
 func create(res http.ResponseWriter, req *http.Request) {
--- session.go
+++ session.go
@@ -1,8 +1,116 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
 
-func isLoggedIn(req *http.Request) bool {
-	cookie, err := req.Cookie()
+	uuid "github.com/satori/go.uuid"
+	"golang.org/x/crypto/bcrypt"
+)
 
+func alreadyLoggedIn(req *http.Request) bool {
+	_, err := req.Cookie("session")
+	if err != nil {
+		return false
+	}
+	return true
+}
+
+func signup(res http.ResponseWriter, req *http.Request) {
+	if req.Method == http.MethodGet {
+		tpl.ExecuteTemplate(res, "signup.gohtml", nil)
+		return
+	}
+
+	u := req.FormValue("username")
+	p := req.FormValue("password")
+	hashPassword, err := bcrypt.GenerateFromPassword([]byte(p), bcrypt.MinCost)
+	if err != nil {
+		http.Error(res, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	q := `
+		INSERT INTO USERS (USERNAME, PASSWORD)
+		VALUES ($1, $2);
+	`
+	result, err := db.Exec(q, u, hashPassword)
+	if err != nil {
+		http.Error(res, err.Error(), http.StatusForbidden)
+		return
+	}
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		http.Error(res, err.Error(), http.StatusForbidden)
+		return
+
+	}
+	if rowsAffected > 0 {
+		tpl.ExecuteTemplate(res, "signup.gohtml", "Account creation successful")
+		return
+	} else {
+		res.Write([]byte("something went wrong."))
+
+	}
+
+}
+
+func login(res http.ResponseWriter, req *http.Request) {
+
+	if req.Method != http.MethodPost {
+		http.Error(res, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+		return
+	}
+
+	username := req.FormValue("username")
+	password := req.FormValue("password")
+	var hashed_password string
+	q := `SELECT PASSWORD FROM USERS WHERE USERNAME = $1`
+	row := db.QueryRow(q, username)
+	if err = row.Scan(&hashed_password); err != nil {
+		tpl.ExecuteTemplate(res, "index.gohtml", "Invalid Username or Password.")
+		return
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(hashed_password), []byte(password))
+	if err != nil {
+		tpl.ExecuteTemplate(res, "index.gohtml", "Invalid Username or Password.")
+		return
+	}
+
+	cookie, err := req.Cookie("session")
+	if err != nil {
+		id := uuid.NewV4()
+		cookie = &http.Cookie{
+			Name:     "session",
+			Value:    id.String(),
+			Secure:   true,
+			HttpOnly: true,
+		}
+		http.SetCookie(res, cookie)
+		_, err = db.Exec("INSERT INTO SESSIONS(sid, username) VALUES ($1, $2)")
+		if err != nil {
+			http.Error(res, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			return
+		}
+	}
+	http.Redirect(res, req, "/pokedex", http.StatusSeeOther)
+
+}
+
+func logout(res http.ResponseWriter, req *http.Request) {
+	cookie, _ := req.Cookie("session")
+
+	// delete session from session table
+	_, err := db.Exec("DELETE FROM SESSIONS WHERE SID = $1", cookie.Value)
+	if err != nil {
+		http.Error(res, err.Error(), http.StatusInternalServerError)
+	}
+	// delete cookie
+	cookie = &http.Cookie{
+		Name:   "session",
+		Value:  "",
+		MaxAge: -1,
+	}
+	http.SetCookie(res, cookie)
+	http.Redirect(res, req, "/index", http.StatusSeeOther)
 }
